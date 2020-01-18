ea2b648be0e472002aa7ebd52bf34f9d0a2a0bf5
Richard Yao <richardyao@Adas-MacBook-Air.local>
2020-01-18 15:10:30 -0800

    Completed Assignment 3: Practicing Directives

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -7,11 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   display = false;
+  clickCount = 0;
   clickLog = [];
   date = new Date();
 
   toggleDisplay() {
     this.display = !this.display;
-    this.clickLog.push({ display: this.display, time: this.date.getTime() });
+    this.clickLog.push({ display: this.display, time: this.date.getTime(), idx: this.clickCount });
+    this.clickCount++;
+    console.log(this.clickCount);
+  }
+  setColor(elm) {
+    return elm.idx > 4 ? "blue" : "white";
   }
 }
